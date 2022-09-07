c06a47a07796b14c2a1149d1befd7b59ba923780
in-social-dev <94141467+in-social-dev@users.noreply.github.com>
2022-09-07 10:57:40 -0400

    Remove drafts

--- is_custom_map.js
+++ is_custom_map.js
@@ -5,6 +5,8 @@ let autocomplete;
 const list = document.querySelector('[data-is-map-list]');
 const zoomResult = 16;
 
+partners_json = partners_json.filter(item => item['draft'] != true);
+
 function initMap() {
     const svgMarkerGreen = {
         path: "M66.9,41.8c0-11.3-9.1-20.4-20.4-20.4c-11.3,0-20.4,9.1-20.4,20.4c0,11.3,20.4,32.4,20.4,32.4S66.9,53.1,66.9,41.8z M37,41.4c0-5.2,4.3-9.5,9.5-9.5c5.2,0,9.5,4.2,9.5,9.5c0,5.2-4.2,9.5-9.5,9.5C41.3,50.9,37,46.6,37,41.4z",
