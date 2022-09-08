e6413dbe8ef8bab46df13ed51972063abeafc9e6
vegTO-dev <112963388+vegTO-dev@users.noreply.github.com>
2022-09-08 15:44:41 -0400

    Update Categories - 2022-09-08T19:44:41.269Z

--- partners-cat.js
+++ partners-cat.js
@@ -1,1 +1,1 @@
-var partner_cat = [{"id":"62fa64f9161d3a4523682634","name":"Restaurants"},{"id":"62fa64f9161d3a5117682626","name":"Groceries & Goods"},{"id":"62fa64f9161d3a395068259c","name":"Wellness"},{"id":"62fa64f9161d3a852768260d","name":"Services"},{"id":"62fa64f9161d3a5009682633","name":"Other"}]
+var partner_cat = [{"id":"62ebebe77adc458474616982","name":"Restaurants"},{"id":"62ebebe77adc457918616981","name":"Groceries & Goods"},{"id":"62ebebe77adc45cf00616980","name":"Wellness"},{"id":"62ebebe77adc45a88361697f","name":"Services"},{"id":"62ebebe77adc4558df616976","name":"Other"}]
